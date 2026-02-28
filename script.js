74b12f878277f2d03cff32805be96612c1ac1b90
simonegreco5 <simoxey.greco@gmail.com>
2026-02-28 09:52:08 +0100

    add part 2 "elaborazione dati"

--- script.js
+++ script.js
@@ -2,12 +2,25 @@
 
 // sezione dati - dati che l'utente deve inserire
 let km_run = prompt('type kilometers')
+console.log(km_run)
 let age = prompt('type your age')
+console.log(age)
 
 // sezione dati - costante prezzo per chilometro
 const km_price = 0.21
 
-// 
+// 2. elaborazione dei dati
+
+// convertiamo i prompt in valori numerici per eseguire le operazioni
+console.log(Number(km_run))
+
+// calcoliamo il prezzo base (senza sconti applicati)
+let full_price = km_price * km_run
+console.log(full_price)
+
+
+
+
 
 
 
