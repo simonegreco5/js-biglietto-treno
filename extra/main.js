fbb2693a34f3d48b119257b0632063900bdeeb87
simonegreco5 <simoxey.greco@gmail.com>
2026-03-02 23:46:25 +0100

    add es.2 extra

--- extra/main.js
+++ extra/main.js
@@ -22,3 +22,45 @@ if (age_marco == age_davide) {
 } else {
     console.log('Davide è più grande di Marco')
 } 
+
+// esercizio extra 2
+
+// 1. inizializzazione 
+// chiediamo all'utente 'pari o dispari' e 'un numero tra 1 e 9'
+
+let scelta_pari_dispari = prompt('scrivi pari o dispari')
+console.log(scelta_pari_dispari);
+
+let numero_utente = Number(prompt('scrivi un numero da 1 a 9'))
+console.log(numero_utente)
+
+// generiamo un numero casuale da 1 a 9 
+let numero_computer = Math.floor(Math.random()* 9) + 1;
+console.log(numero_computer)
+
+// sommiamo i numeri scelti dall'utente con quelli generati dal computer
+let somma_numeri = numero_utente + numero_computer
+console.log(somma_numeri);
+
+// 2. condizione 1
+// verifichiamo se la somma dei numeri e pari o dispari e poi la comunichiamo
+// denomianiamo la variabile risultato fuori dal block(if/else) condizionale cosi 
+// da poter modifcare il valore (e richiamarla) anche all'interno block(if/else)
+let risultato;
+
+if (somma_numeri % 2 === 0) {
+    risultato = 'pari'
+} else {
+    risultato = 'dispari'
+}
+
+// condizione 2 
+// usiamo nuovamente (if/else) per confrontare 'risultato' con la scdlta iniziale 
+// per stabilire se l'utente ha vinto o perso
+
+if (risultato === scelta_pari_dispari) {
+    console.log('HAI VINTO')
+} else {
+    console.log('hai perso')
+}
+
